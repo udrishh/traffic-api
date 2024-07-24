2025cbfecbff363fe2dfd71744a8fd4a781470dd
udrishh <udris.bogdan9@gmail.com>
2024-07-24 10:30:10 +0300

    Changed project structure

--- TrafficAPI/TrafficAPI/UserInformation.cs
+++
@@ -1,8 +0,0 @@
-namespace TrafficAPI;
-
-public class UserInformation
-{
-    public string IpAddress { get; set; }
-    public string UserAgent { get; set; }
-    public DateTime RequestDate { get; set; }
-}
