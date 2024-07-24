0f6ce695d74113e59481736a5cc0b3ae5af2d0fd
udrishh <udris.bogdan9@gmail.com>
2024-07-24 10:31:14 +0300

    Added unversioned files

---
+++ TrafficAPI/UserInformation.cs
@@ -0,0 +1,8 @@
+namespace TrafficAPI;
+
+public class UserInformation
+{
+    public string IpAddress { get; set; }
+    public string UserAgent { get; set; }
+    public DateTime RequestDate { get; set; }
+}
